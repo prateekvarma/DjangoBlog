0aa1443b9e78eaf0f9532785c0dc54586ea73e19
prateekvarma <prtk.varma@gmail.com>
2020-10-30 11:28:48 -0600

    added filters for status, created, publish and author on admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -5,3 +5,4 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')

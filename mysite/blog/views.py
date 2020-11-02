784eb4caf928e53a729607d489a5fec0cf56bdb9
prateekvarma <prtk.varma@gmail.com>
2020-11-02 11:51:58 -0600

    created new view function, using the 'published' custom manager, to get all entries

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    #Below, the render method takes the (request object, template path, variable to render)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})

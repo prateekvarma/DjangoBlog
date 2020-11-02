9e184c71cb470e120f50db099f2b2dc569ba5524
prateekvarma <prtk.varma@gmail.com>
2020-11-02 15:16:11 -0600

    added get_absolute_url() to blog model for canonical urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -28,6 +28,13 @@ class Post(models.Model):
     published = PublishedManager() #our custom manager
     #custom manager code ends
 
+    #adding canonical URLs
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                        self.publish.month,
+                        self.publish.day, self.slug])
+
 class Meta:
     ordering = ('-publish',)
 

c3feb5690aba8b5d0bff84e5bc392c956c27cb95
prateekvarma <prtk.varma@gmail.com>
2020-11-06 15:04:53 -0600

    modified post_detail view to get the post and save the comment

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 # Create your views here.
@@ -37,9 +37,32 @@ def post_detail(request, year, month, day, post):
                             publish__year = year,
                             publish__month = month,
                             publish__day = day)
+
+    # List of active comments for this post
+    #The .comments. below is "related_name='comments'" from Comments model to refer to the relationship
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # a comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #create comment object, but not store in DB yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the related post to comment
+            new_comment.post = post
+            #save comment to DB
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                  'blog/post/detail.html',
-                 {'post': post})
+                 {'post': post,
+                 'comments': comments,
+                 'new_comment': new_comment,
+                 'comment_form': comment_form})
 
 def post_share(request, post_id):
     #retrieve post by id

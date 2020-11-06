c4eb091c84f2bf3c49f6f5d82e065eb31af2c939
prateekvarma <prtk.varma@gmail.com>
2020-11-06 10:19:15 -0600

    fixed some indentations, includes and return codes

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -49,16 +50,17 @@ def post_share(request, post_id):
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-        # Form fields passed validation
-        cd = form.cleaned_data
-        # ... send email
-        post_url = request.build_absolute_uri(post.get_absolute_url())
-        subject = f"{cd['name']} recommends you read " \
-        f"{post.title}"
-        message = f"Read {post.title} at {post_url}\n\n" \
-        f"{cd['name']}\'s comments: {cd['comments']}"
-        send_mail(subject, message, 'admin@myblog.com',[cd['to']])
-        sent = True
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+            f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+            f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com',[cd['to']])
+            sent = True
+            return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
     else:
-    form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+        form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
